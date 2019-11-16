8c70c7ccef2c515da2d7233a8d1e1e26c7b16ea8
Lawrence <Caulker>
2019-11-15 22:48:26 -0500

    added navigation setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,46 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+// Web Pages
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import Contact from './pages/ContactPage';
+import SingleProduct from './pages/SingleProductPage';
+import Cart from './pages/CartPage';
+import Default from './pages/Default';
+
+import {Route, Switch} from 'react-router-dom';
+
+
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import SideCart from './components/SideCart';
+import Footer from './components/Footer';
+
+
 class App extends Component {
   render() {
-    return <h1>Hello from CellPhone Store</h1>
+    return <>
+     {/* Navbar, Sidebar, Cart & Footer Components */}
+     <Navbar />
+     <Sidebar />
+     <SideCart />
+     
+     <Switch>
+       <Route path="/" exact component={ Home } />
+       <Route path="/about" component={ About } />
+       <Route path="/contact" component={ Contact } />
+       <Route path="/products" exact component={ Products } />
+       <Route path="/products/:id" component={ SingleProduct } />
+       <Route path="/cart" component={ Cart } />
+       <Route component={ Default } />
+     </Switch>
+
+     <Footer />
+    </>
   }
 }
+
 
 export default App;
---
+++ src/components/SideCart.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SideCart() {
+  return (
+    <div>Hello from SideCart</div>
+  )
+}
