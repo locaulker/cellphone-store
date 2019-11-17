9c39dfd1eadab230b08780cc8761e47aa07dfedd
Lawrence <Caulker>
2019-11-17 10:23:06 -0500

    footer with social icons completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {ProductConsumer} from '../context';
+
 
 export default function Footer() {
   return (
-    <>Hello from Footer</>
+    <ProductConsumer>
+      {value => {
+        return (
+          <FooterWrapper>
+            <div className="container py-3">
+              <div className="row">
+                <div className="col-md-6">
+                  <p className="pt-2">
+                    Copyright &copy; &ndash; <strong>eStore {new Date().getFullYear()}</strong> | <small>All Rights Reserved</small>
+                  </p>
+                </div>
+                <div className="col-md-6 text-right">
+                  {value.socialIcons.map(item => <a href={item.url} key={item.id} target="_blank">{item.icon}</a>)}
+                </div>
+              </div>
+            </div>
+          </FooterWrapper>
+        )
+      }}
+    </ProductConsumer>
   )
 }
+
+
+const FooterWrapper = styled.footer`
+  background: var(--mainBlack);
+  color: var(--mainWhite);
+
+  .icon {
+    font-size: 2.5rem;
+    color: var(--mainWhite);
+    transition: var(--mainTransition);
+    padding-left: 1rem;
+  }
+  .icon:hover {
+    color: var(--primaryColor);
+    cursor: pointer;
+  }
+`;
